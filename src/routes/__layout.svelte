714c08caa538e7cc8c5688003a84eaee3f8ef21b
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2021-11-23 12:40:36 +0100

    fix: added robots meta tag for non-production builds

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
 	import Footer from '$lib/footer/Footer.svelte';
 	import '../app.css';
+
+	const isProd = import.meta.env.VITE_PROD === 'true';
 </script>
+
+<svelte:head>
+	{#if !isProd}
+		<meta name="robots" content="noindex,noflollow" />
+	{/if}
+</svelte:head>
 
 <main>
 	<slot />
